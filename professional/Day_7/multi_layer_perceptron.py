cff7fb805ebf37570c1e66da4f1dcd9d8b5ba5bd
intel <iftihorbekd@gmail.com>
2026-03-04 22:09:05 +0500

    feat: Project added

---
+++ professional/Day_7/multi_layer_perceptron.py
@@ -0,0 +1,19 @@
+import numpy as np 
+class MLP:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.W1 = np.random.randn(input_size, hidden_size)
+        self.b1 = np.zeros((1, hidden_size))
+
+        self.W2 = np.random.randn(hidden_size, output_size)
+        self.b2 = np.zeros((1, output_size))
+
+    def relu(self, x):
+        return np.maximum(0, x)
+
+    def forward(self, X):
+        self.z1 = np.dot(X, self.W1) + self.b1 
+        self.a1 = self.relu(self.z1)
+
+        self.z2 = np.dot(self.a1, self.W2) + self.b2
+
+        return self.z2
