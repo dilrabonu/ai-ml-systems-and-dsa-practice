80a6d432e16d57f677938b40a2e1eb2bac3b2c98
intel <iftihorbekd@gmail.com>
2026-05-08 10:19:57 +0500

    min stack added

--- dsa/stack/valid_parentheses.py
+++ dsa/stack/valid_parentheses.py
@@ -1,3 +1,6 @@
+from sympy import false
+
+
 def is_valid(s: str) -> bool:
     pairs = {")": "(", "]" : "[", "}" : "{"}
     stack: list[str] = []
@@ -10,3 +13,15 @@ def is_valid(s: str) -> bool:
                 return False
 
     return not stack
+def is_valid(s) -> bool:
+    pairs =  {")": "(", "]": "[", "}": "{"}
+    stack: list[str] = []
+    for ch in s:
+        if ch in "([{":
+            stack.append(ch)
+        elif ch in pairs:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+    return not stack
+
+
