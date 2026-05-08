d7986204a216f668d160b14629e5a5909a5f6066
intel <iftihorbekd@gmail.com>
2026-05-08 10:01:32 +0500

    Valid parentheses implemented

---
+++ dsa/stack/valid_parentheses.py
@@ -0,0 +1,12 @@
+def is_valid(s: str) -> bool:
+    pairs = {")": "(", "]" : "[", "}" : "{"}
+    stack: list[str] = []
+
+    for ch in s:
+        if ch in "([{":
+            stack.append(ch)
+        elif ch in pairs:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+
+    return not stack
