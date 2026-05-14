e68a50fe3bbca4c9f89a9fbd8617527da3afe22c
intel <iftihorbekd@gmail.com>
2026-05-14 15:28:37 +0500

    Updated

--- dsa/python/py.py
+++ dsa/python/py.py
@@ -7,3 +7,10 @@ print(even)
 
 toq = [n for n in range(1, 11) if n  % 2 != 0]
 print(toq)
+
+even = [n for n in range(2,22) if n % 2 == 0]
+print(even)
+
+# lambda:
+square = lambda x: x ** 2
+print(square(5))
