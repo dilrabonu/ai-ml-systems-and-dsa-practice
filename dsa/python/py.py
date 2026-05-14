b9c5a09da3a819944ced22312098ae960375d0ef
intel <iftihorbekd@gmail.com>
2026-05-14 15:10:57 +0500

    Update

--- dsa/python/py.py
+++ dsa/python/py.py
@@ -1,3 +1,9 @@
 fruits = ["apple", "banana", "strawberry"]
 comp = [f.upper() for f in fruits]
 print(comp)
+
+even = [n for n in range(1, 11) if n % 2 == 0]
+print(even)
+
+toq = [n for n in range(1, 11) if n  % 2 != 0]
+print(toq)
