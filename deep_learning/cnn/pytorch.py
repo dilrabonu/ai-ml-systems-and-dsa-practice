4413db62ab6206683daefe7c54206256ae81b6d6
intel <iftihorbekd@gmail.com>
2026-05-19 21:54:43 +0500

    Updated

--- deep_learning/cnn/pytorch.py
+++ deep_learning/cnn/pytorch.py
@@ -1,6 +1,12 @@
 import torch 
+import torch.nn as nn
 
 x = torch.tensor([1.0, 2.0, 3.0], requires_grad=True)
 y = (x ** 2).sum()
 y.backward()
 print(x.grad)
+
+class SimpleClassifier(nn.Module):
+    def __init__(self, in_dim: int, hidden: int, n_classes: int):
+        super().__init__()
+        
