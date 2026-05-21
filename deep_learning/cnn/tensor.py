94d73e60fea85539fcc097c471b1c3ee1b4bf943
intel <iftihorbekd@gmail.com>
2026-05-21 19:35:41 +0500

    Updated

--- deep_learning/cnn/tensor.py
+++ deep_learning/cnn/tensor.py
@@ -6,3 +6,7 @@ b = torch.tensor([[1.0, 2.0], [3.0, 4.0]])  # matrix (2*2)
 
 print(a.shape)
 print(b.shape)
+
+print(a + 1) # 1 adds for each elements
+print(a * 2) # 2 multiplies for each elements
+print(b @ b) # matrix multiplication
