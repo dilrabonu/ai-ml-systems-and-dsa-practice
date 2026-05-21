4874ee50e1e2e504cdd2878f51cdfd3d4d919260
intel <iftihorbekd@gmail.com>
2026-05-21 20:03:39 +0500

    Updated

--- deep_learning/cnn/full_model.py
+++ deep_learning/cnn/full_model.py
@@ -6,4 +6,12 @@ class MyModule(nn.Module):
         super().__init__()
         self.linear1 = nn.Linear(4, 8)  # 4 - in, 8- out
         self.linear2 = nn.Linear(8, 1)  # 8 - in, 1- out
-        
+
+    def forward(self, x):
+        x = self.linear1(x)  # pass linear 1
+        x = torch.relu(x)    #ReLU activation (gradient flow)
+        x = self.linear2(x)   # pass linear 2
+        return x
+
+model = MyModule()
+print(model)
