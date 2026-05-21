0452c123f7133f98f03e016d5d5e30cfc54590d2
intel <iftihorbekd@gmail.com>
2026-05-21 20:01:16 +0500

    Updated

--- deep_learning/cnn/full_model.py
+++ deep_learning/cnn/full_model.py
@@ -1,3 +1,9 @@
 import torch
 import torch.nn as nn 
 
+class MyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(4, 8)  # 4 - in, 8- out
+        self.linear2 = nn.Linear(8, 1)  # 8 - in, 1- out
+        
