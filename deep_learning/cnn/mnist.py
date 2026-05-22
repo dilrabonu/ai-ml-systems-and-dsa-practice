31ccfd22b11f5073665a0e4e4bcd5d987ef87239
intel <iftihorbekd@gmail.com>
2026-05-22 21:07:43 +0500

    Updated

--- deep_learning/cnn/mnist.py
+++ deep_learning/cnn/mnist.py
@@ -3,5 +3,17 @@ Aim: Classify MNIST pictures with hand written digits
 Model train the picture and tell which digit is it?
 
 Dataset:
+- 60000 training images
+- 10000 test images
+- 28x28 pixels
+- 1 channel (grayscale)
 
+Steps:
+1. Import Libraries
+2. Choose Device GPU/CPU
+3. Settings Hyperparameters
+4. Load Data
+5. Create Model
+6. Loss function and optimizer
+7. 
 """
