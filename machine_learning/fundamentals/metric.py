23a8b2e4acc5bb012703ecf874be1ac45c41b5bb
intel <iftihorbekd@gmail.com>
2026-05-25 21:40:23 +0500

    New metrics started

---
+++ machine_learning/fundamentals/metric.py
@@ -0,0 +1,11 @@
+"""
+Bashorat
+                  Sog'lom  Kasal
+Haqiqat Sog'lom    TN      FP      <- FP = noto'g'ri "kasal" dedi (xavotir)
+        Kasal      FN      TP      <- FN = kasalni "sog'lom" dedi (XAVFLI!)
+
+TN = True Negative (sog'lomni sog'lom dedi)
+TP = True Positive (kasalni kasal dedi)  
+FP = False Positive (sog'lomni kasal dedi) — TYPE I ERROR
+FN = False Negative (kasalni sog'lom dedi) — TYPE II ERROR
+""" 
