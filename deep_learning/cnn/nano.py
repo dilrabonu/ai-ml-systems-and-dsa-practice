79223fe7e75aa64c28a7d18f9a4c44cda318e841
intel <iftihorbekd@gmail.com>
2026-06-02 21:14:18 +0500

    Layer Norm implemented

--- deep_learning/cnn/nano.py
+++ deep_learning/cnn/nano.py
@@ -6,3 +6,14 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F 
+
+# Layer Norm
+class LayerNorm(nn.Module):
+
+    def __init__(self, ndim, bias):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
