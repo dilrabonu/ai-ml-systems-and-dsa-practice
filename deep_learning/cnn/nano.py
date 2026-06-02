475cae133b8239fab15be05d13c8fb5efcc131a3
intel <iftihorbekd@gmail.com>
2026-06-02 21:40:15 +0500

    Updated the project

--- deep_learning/cnn/nano.py
+++ deep_learning/cnn/nano.py
@@ -22,3 +22,10 @@ class CausalselfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        #regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
