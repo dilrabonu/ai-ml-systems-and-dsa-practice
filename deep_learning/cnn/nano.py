efc15808ab4a6659c4ab0766c83ef432e4ff847b
intel <iftihorbekd@gmail.com>
2026-06-03 21:09:11 +0500

    Updated

--- deep_learning/cnn/nano.py
+++ deep_learning/cnn/nano.py
@@ -117,3 +117,8 @@ class GPT(nn.Module):
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.transformer.wte.weight = self.lm_head.weight
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
