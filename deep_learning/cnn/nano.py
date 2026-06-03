62f0cbded40d66c386c322f081236e6583790dfd
intel <iftihorbekd@gmail.com>
2026-06-03 21:26:14 +0500

    Updated

--- deep_learning/cnn/nano.py
+++ deep_learning/cnn/nano.py
@@ -122,3 +122,10 @@ class GPT(nn.Module):
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
                 torch.nn.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+                print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+    
+    def get_num_params(self, non_embedding=True):
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
