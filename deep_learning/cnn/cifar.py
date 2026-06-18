3a91bd31662d8b8af78a4ee98d9afaf2d085ebce
intel <iftihorbekd@gmail.com>
2026-06-19 00:01:33 +0500

    Step 11 has implemented

--- deep_learning/cnn/cifar.py
+++ deep_learning/cnn/cifar.py
@@ -201,3 +201,4 @@ for epoch in range(1, EPOCHS + 1):
 
     print(f"Epoch {epoch:2d}/{EPOCHS} | Loss: {train_loss:.3d} |"
           f"Train accuracy: {tarin_acc:.2d}% | Test accuracy: {test_acc:.2f}%")
+print("------- Training Finished----\n")
